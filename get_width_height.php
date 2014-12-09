64dffe5d7e2db5fa0e1e5850fc69d6bb0c7e5547
gaby22v <patrascugabriel@hotmail.com>
2014-12-09 21:40:29 +0200

    Create get_width_height.php

---
+++ get_width_height.php
@@ -0,0 +1,8 @@
+<?php
+list($width, $height, $type, $attr) = getimagesize("apache_pb.gif");
+echo "Inaltime:" .$width;
+echo "<BR>";
+echo "Latime:" .$height;
+echo "<BR>";
+echo "Tip imagine:" .$type;
+?>
